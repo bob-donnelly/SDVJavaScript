6edfd1b399a68c92fdeb905765cb7058b7e11b3b
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-03-09 10:25:15 +1300

    added comments

--- src/week3.js
+++ src/week3.js
@@ -1,8 +1,20 @@
+/* const is a variable declaration for "constant"
+
+Declared constant w for 'world' in hellow world*/
+
 const w = 'hello world'
+
+// Constant declared g for greeting 'welcome'
 
 const g = 'welcome'
 
+// Constant declared c for 'check'
+
 const c = 'check'
+
+/* console.log() prints the variable inside the brackets 
+viewable with quokka extension or nodemon with nodemon 
+file name example - index.js while in the folder contatining them */
 
 console.log(w)
 
