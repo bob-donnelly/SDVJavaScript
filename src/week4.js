dfa65efb2041b85a5f56d25c9680b03dd1cb0e86
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-03-15 12:31:09 +1300

    Declared variables and initialised.

---
+++ src/week4.js
@@ -0,0 +1,16 @@
+// REVIEW 
+
+const { post } = require("got")
+
+// values and variables
+
+let country = "New Zealand"
+let city = "Nelson"
+let postcode = "7010"
+
+console.log(country)
+console.log(city)
+console.log(postcode)
+
+console.log(country + ' ' + city + ' ' + postcode)
+
