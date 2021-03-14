5c4b4518fb465b0ac3a3c4edba52109bcb108535
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-03-15 12:50:27 +1300

    Added commments for typeof keyword.

--- src/week4.js
+++ src/week4.js
@@ -14,3 +14,19 @@ console.log(postcode)
 
 console.log(country + ' ' + city + ' ' + postcode)
 
+// Datatypes
+
+let isIsland = true
+
+// Variable is declared but not initialised yet.
+
+let language 
+
+console.log(isIsland)
+console.log(language)
+
+// typeof is a reserved keyword that shows the datatype of the declared variable.
+// reserved keywords are for javascript to parse differently to variables, you cannot use them.
+
+console.log(typeof isIsland)
+
