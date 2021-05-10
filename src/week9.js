2b96ef5e204bb8019da6e493ec7220969e6727e5
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-05-10 13:28:18 +1200

    Flatten, reduce and mapping depth of arrays.

---
+++ src/week9.js
@@ -0,0 +1,53 @@
+// Function that takes two numbers and a mathematical operator.
+
+// Answer
+
+function calFunc(num, operator, num2) {
+operations = {
+    '+': (a,b) => a + b,
+    '-': (a,b) => a - b,
+    '*': (a,b) => a * b,
+    '/': (a,b) => a / b
+}
+return operations[operator] (num, num2)
+}
+
+console.log(calFunc(4, '+', 3))
+console.log(calFunc(4, '-', 3))
+console.log(calFunc(4, '*', 3))
+console.log(calFunc(4, '/', 3))
+
+// Return the sum of an array.
+
+function sumFunc(arr) {
+    return arr.flat(Infinity).reduce((a,b) => a+b);
+}
+
+console.log(sumFunc([1,2,4,4,6,8,7, [3,4,5,6,7,8,8]]))
+
+// const flatten = (arr) => {
+// const result = [];
+
+// arr.forEach(element => {
+//     Array.isArray(element) ? result.push(...flatten(element)) : result.push(element)
+// })
+// }
+
+// console.log(flatten([[1,2,3,4], [2,3,4,6], 2,3,4,5]))
+
+const getDepth = obj => {
+    let level = 1
+
+    Object.keys(obj).forEach(key => {
+        if(typeof obj[key] === 'object') {
+            const depth = getDepth(obj[key] + 1,
+                
+                level = Math.max(depth, level)
+                )
+        }
+    })
+    return level
+}
+
+console.log(getDepth([]))
+
