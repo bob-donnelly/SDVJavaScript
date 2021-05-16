0fda4d2e9fef96714840b35d9b5bee8914b1c0db
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-05-16 20:00:27 +1200

    Extra array manipulation.

--- src/extra.js
+++ src/extra.js
@@ -1,6 +1,6 @@
 function element_replace() {
 
-    let arr = Array(0, 1, 1, 0,);
+    let arr = [0, 1, 1, 0,];
 
     // ele[ele.map((x, i) => [i, x]).filter(
     // x => x[1] == 1)[0][0]] = 'x'
@@ -8,8 +8,11 @@ function element_replace() {
     // ele[ele.map((x, i) => [i, x]).filter(
     //     x => x[1] == 1)[0][0]] = 'x'
 
-    //     ele[ele.map((x, i) => [i, x]).filter(
-    //         x => x[1] == 1)[0][0]] = 'x'
+        // arr[arr.map((x, i) => [i, x]).filter(
+        //     x => x[1] == 1)[0][0]] = 'x'
+
+        arr.map(arr => [0, 1]).filter(arr => arr[1] == 1)[1][2] = 'x';
+        console.log(arr)
 
     // arr = ele.map(x =>  {
 
@@ -20,62 +23,61 @@ function element_replace() {
     // (n === 0) ? [] :
     // [n-1, 'x']) 
 
-    arr = arr.flatMap( (n) =>
-    // (n === 0) ? [] :
-    [n=0, 'x', n=0, 'x']) 
-    
-
-console.log(arr)
+    // arr = arr.flatMap( (n) =>
+    // // (n === 0) ? [] :
+    // [n=0, 'x', n=0, 'x']) 
+    console.log(arr)
 }
-;
-element_replace()
-
-function oneTox() {
-    
-}
-
-// let board = []
-// let row = []
-// let random = (a) =>Math.floor(Math.random() *Math.floor(a)) // function to generate random number
-// let bombNumber=random(4) +Math.floor(4) //chosing random number of bomb this is optional as the function below can add bomb without this variable
-// let bombLocation= []
-
-// for (let i = 0; i < 4; i++) {
-//     for (let j = 0; j < 4; j++) {
-//         row.push(0)
-//     }
-//     board.push(row)
-//     row = []
-// }
-
-// //generate bomb
-// function bombGenerator(para = 5) {
-//     let newBoard = [...board]
-//         //adding para number of bomb to the board with row and column. The default is 5
-//     for (let i = 0; i < para; i++) {
-//         let row = random(4)
-//         let column = random(4)
-//         newBoard[row][column] = 1
-//     }
-//     return newBoard
-// }
 
 let arr = [  [0, 1, 0, 0],  [0, 0, 1, 0],  [0, 1, 0, 1],  [1, 1, 0, 0],];
 
-arr = arr.flatMap( (n) =>
+arr.map(arr => [0, 1]).filter(arr => arr[1] == 1)[1][2] = 'x'
 
-[n=0, 'x', n=0, 'x']
+console.log(arr)
+
+arr = arr.flatMap( (x) =>
+
+[x=0, 'x']
+
+) 
+
+
+console.log(arr)
+
+for(x = 0; x < arr.length; x ++) {
+
+    console.log(arr[x])
+    }
+
+console.log(arr)
+
+// function loop(arr) {
+    
+//     arr = arr.flatMap( (n) =>
+    
+//     [n=0, 'x', n=0, 'x']
+    
+//     ) 
+//     console.log(arr)
+    
+// }
+
+
+arr = arr.flatMap( (x) =>
+
+[[x=0, 'x', x=0, x=0], [x=0, x=0, 'x', x=0],]
 
 ) 
 
 console.log(arr)
 
-function createDepth(arr) {
-    if(arr.length === 1) return arr
-    return [arr[0], createDepth(arr.slice(1))]
-}
 
-console.log(createDepth(arr))
+// function createDepth() {
+//     if(arr.length === 1) return arr
+//     return [arr[0], createDepth(arr.slice(1))]
+// }
+
+// console.log(createDepth([1,2,3,5,6]))
 
 // let myArr = [];
 // myArr.push(arr)
@@ -89,3 +91,4 @@ console.log(createDepth(arr))
 //  myArr = Object.entries(obj);
 
 // console.log(myArr)
+
