e21796950f2e9cd7e250c49fbedc5be1f662de17
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-05-18 09:54:04 +1200

    Week3 merged.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
 
+=======
+# SDVJavaScript
+
+## Week 1
+
+This week we set up our work environment to prepare for the lessons we are going to be learning during this course. We also learnt how to print with console.log() Hello World! to get us started with Javascript.
+
+``` Code snippets work with 3 backticks on each side of the paragraph ```
+
+## Week 2
+
+This week we are learning how git branches are created and managed in terms of version control for my classes. We have used GitKraken to check our branches and how we have structured our branches, if they are correct or clean.
+>>>>>>> week3
---
+++ src/week3.js
@@ -0,0 +1,60 @@
+//TODO Javascript Variables
+
+/* const is a variable declaration for "constant"
+
+Declared constant w for 'world' in hello world*/
+
+const w = 'hello world'
+
+// Constant declared g for greeting 'welcome'
+
+const g = 'welcome'
+
+// Constant declared c for 'check'
+
+const c = 'check'
+
+/* console.log() prints the variable inside the brackets 
+viewable with quokka extension or nodemon with nodemon 
+file name example - index.js while in the folder contatining them */
+
+console.log(w)
+
+console.log(g)
+
+console.log(c)
+
+/* Variables or var were the first containers there are const and let now
+const is constant and fufills the same function as var which is set, 
+can't be changed | let is a container that can be used mutliple times 
+for different strings / values */
+
+var fn = 'Bob'
+
+var ln = ' Win-Donnelly'
+
+console .log(fn,ln)
+
+// Let can assign another value but you do not need to declare let again
+
+let lastName = 'Kahwaji'
+
+console.log(lastName)
+
+lastName = 'Win-Donnelly'
+
+console.log(lastName)
+
+// Using a let variable to add two previous values together.
+
+let fullName = fn + ' ' + ln 
+
+console.log(fullName)
+
+// added strings to call first name and last name to add a sentence to it.
+
+fullName = `My fullname is  ${fn} ${ln}`
+
+console.log(fullName)
+
+
