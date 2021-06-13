b33917ebd2b2fed7f43489d8cb1c3ad806c327bf
bob-donnelly <[bob-donnelly@live.nmit.ac.nz]>
2021-06-13 14:18:19 +1200

    Updating dependencies.

--- src/nestedLoop.js
+++ src/nestedLoop.js
@@ -1,9 +1,25 @@
-let rating = [0][0];
+// let rating = [0][0];
 
-for(let row = 0; row < rating.length; row++) {
-    for(let col = 0; col < rating[0].length; col++) {
-        console.log(col)
+// for(let row = 0; row < rating.length; row++) {
+//     for(let col = 0; col < rating[0].length; col++) {
+//         console.log(col)
+//     }
+//     console.log(row)
+// }
+
+function exampleJavaScript(arr) {
+	let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    let largest = arr[i][0];
+    for(let j = 0; j < arr[i].length; j++){
+      if(arr[i][j] > largest) {
+        largest = arr[i][j]
+      }
     }
-    console.log(row)
+    newArr.push(largest)
+  }
+  return newArr
 }
+
+console.log(exampleJavaScript([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]])) 
 
